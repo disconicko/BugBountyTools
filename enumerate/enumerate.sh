adfe9f40e716f54fd5007c6ef83ec179ee659616
disconicko <nicholas@foralleternity.net>
2024-01-05 10:21:45 +1100

    [UPDATE] Added Output Arguement

--- enumerate/enumerate.sh
+++ enumerate/enumerate.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#TODO Add Proxy wrapper
-#TODO Port scan
-
 main () {
-    while getopts ':a:c:m:' flag; do
+    while getopts ':a:c:m:o:' flag; do
         case "${flag}" in 
             a) 
                 asn="${OPTARG}" ;;
@@ -12,6 +9,8 @@ main () {
                 cidr="${OPTARG}" ;;
             m)
                 mode="${OPTARG}" ;;
+            o)
+                output="${OPTARG}" ;;
             *) 
                 print_usage
                 exit 1 ;;
@@ -21,73 +20,87 @@ main () {
     initializeVariables
 
     # Phase 1 Enumeration
-    echo -e "$redOpen Starting Phase 1 - Enumeration on $target $redClose"
+    echo -e "$redOpen Starting Phase 1 - Enumeration on $target $colourClose"
     asnEnum
     subdomainEnum
     githubEnum
     wayBackUrls
     httpResolve
     crawl
-    echo -e "$redOpen Finished Phase 1 - Enumeration on $target $redClose"
+    echo -e "$redOpen Finished Phase 1 - Enumeration on $target $colourClose"
     
     # Phase 2 Information Gathering
-    echo -e "$redOpen Starting Phase 2 - Information Gathering on $target $redClose"
+    echo -e "$redOpen Starting Phase 2 - Information Gathering on $target $colourClose"
     iisDiscovery
     getHeaders
     serviceScan
     #portScan
-    echo -e "$redOpen Finished Phase 2 - Information Gathering on $target $redClose"
+    #commonPortScan
+    echo -e "$redOpen Finished Phase 2 - Information Gathering on $target $colourClose"
 
     # Phase 3 Vulnerability Scanning
-    echo -e "$redOpen Starting Phase 3 - Vulnerability Scanning on $target $redClose"
-    nucleiScan 
+    echo -e "$redOpen Starting Phase 3 - Vulnerability Scanning on $target $colourClose"
+    #nucleiScan 
     #niktoScan
-    echo -e "$redOpen Finished Phase 3 - Vulnerability Scanning on $target $redClose"
-    echo -e "$redOpen Script finished - Happy Hacking $redClose"
+    echo -e "$redOpen Finished Phase 3 - Vulnerability Scanning on $target $colourClose"
+    echo -e "$redOpen Script finished - Happy Hacking $colourClose"
 }
 
 print_usage() { 
-    printf "Usage: enumerate.sh -a [ASN] -c [CIDR] -m [passive|active]\n"
+    printf "Usage: enumerate.sh -a [ASN] -c [CIDR] -m [passive|active] -o [Output Folder]\n"
 }
 
 initializeVariables(){
-    #Directories for file storage
-    mkdir Enumeration 2>/dev/null
-    mkdir InformationGathering 2>/dev/null
-    mkdir VulnerabilityScanning 2>/dev/null
 
     # Files for output
-    currentDirectory=$(pwd)
+    if [[ -n $output]]; do
+        printf "Please provide a target output directory"
+        print_usage
+        quit
+    fi
+
+    outputDirectory=$output
     target="Example Target"
-    topLevelDomains="$currentDirectory/topLevelDomains.txt"
-    subdomains="$currentDirectory/Enumeration/subDomains.txt"
-    hosts="$currentDirectory/Enumeration/hosts.txt"
-    unconfirmedTopLevelDomains="$currentDirectory/InformationGathering/unconfirmedTopLevelDomains.txt"
-    iisServers="$currentDirectory/InformationGathering/iisServers.txt"
-    nmapOutput="$currentDirectory/InformationGathering/nmap.out"
-    vulnerabilities="$currentDirectory/VulnerabilityScanning/vulnerabilities.txt"
+    topLevelDomains="$outputDirectory/topLevelDomains.txt"
+    subdomains="$outputDirectory/Enumeration/subDomains.txt"
+    hosts="$outputDirectory/Enumeration/hosts.txt"
+    unconfirmedTopLevelDomains="$outputDirectory/InformationGathering/unconfirmedTopLevelDomains.txt"
+    iisServers="$outputDirectory/InformationGathering/iisServers.txt"
+    nmapOutput="$outputDirectory/InformationGathering/nmap.out"
+    vulnerabilities="$outputDirectory/VulnerabilityScanning/vulnerabilities.txt"
 
     # Files for outputting custom wordlists
-    paths="$currentDirectory/InformationGathering/pathsWordlist.txt"
-    variables="$currentDirectory/InformationGathering/variablesWordlist.txt"
+    paths="$outputDirectory/InformationGathering/pathsWordlist.txt"
+    variables="$outputDirectory/InformationGathering/variablesWordlist.txt"
 
     # Variables
     rateLimit="100"
-    commonPorts="80,443,8080,8443,8000,8081,8008,8888,8082,81,8083,82,8084,8001,8090,83,8085,8088,8002,8089,84,8091,85,8003,8092,8004,8093,86,8094,8005,8095,8006,8096,8007,8097,8009,8098,88,8099,8010,8100,8011,8101,8012,8102,8013,8103,8014,8104,8015"
+    commonPorts="8080,8443,8000,8081,8008,8888,8082,81,8083,82,8084,8001,8090,83,8085,8088,8002,8089,84,8091,85,8003,8092,8004,8093,86,8094,8005,8095,8006,8096,8007,8097,8009,8098,88,8099,8010,8100,8011,8101,8012,8102,8013,8103,8014,8104,8015"
     redOpen="\033[31m"
-    redClose="\033[0m"
+    colourClose="\033[0m"
+    greenOpen="\033[32m"
     gitToken=""
+
+    #Directories for file storage
+    mkdir $outputDirectory/Enumeration 2>/dev/null
+    mkdir $outputDirectory/InformationGathering 2>/dev/null
+    mkdir $outputDirectory/VulnerabilityScanning 2>/dev/null    
+
+    #Print Banner
+    echo -e "   ______                ______           __ \n  / ____/_______  ____  / ____/___ ______/ /_\n / / __/ ___/ _ \/ __ \/ /_  / __ \`/ ___/ __/\n/ /_/ / /  /  __/ /_/ / __/ / /_/ (__  ) /_  \n\\____/_/   \\___/ .___/_/    \\__,_/____/\\__/  \n              /_/                            "                     
+    
 }
 
 asnEnum() {
-    echo -e "$redOpen Starting passive ASN enumeration on $target $redClose"
+    echo -e "$redOpen Starting Passive ASN enumeration $colourClose"
     amass intel -asn $asn | anew $topLevelDomains
+    echo -e "$greenOpen Finished Passive ASN Enumeration $colourClose"
 }
 
 subdomainEnum(){
-    echo -e "$redOpen Starting passive subdomain enumeration on $target $redClose"
+    echo -e "$redOpen Starting Passive Subdomain Enumeration $colourClose"
     while IFS= read -r domain; do
-        echo -e "$redOpen Starting subdomain enumeration on $domain $redClose"
+        echo -e "$redOpen Performing Passive Subdomain Enumeration On $domain $colourClose"
 
         # Run each command with a timeout in the background
         timeout 5m subfinder -d "$domain" -silent | grep -v '[@*:]' | grep "$domain$" | anew $subdomains &
@@ -109,28 +122,30 @@ subdomainEnum(){
             if [ $exit_status -eq 124 ]; then
                 case $pid in
                     $pid_subfinder)
-                        echo "Subfinder timed out."
+                        echo -e "$redOpen Subfinder timed out. $colourClose"
                         ;;
                     $pid_amass_enum)
-                        echo "Amass enum timed out."
+                        echo -e "$redOpen Amass enum timed out. $colourClose"
                         ;;
                     $pid_amass_intel)
-                        echo "Amass intel timed out."
+                        echo -e "$redOpen Amass intel timed out. $colourClose"
                         ;;
                     $pid_assetfinder)
-                        echo "Assetfinder timed out."
+                        echo -e "$redOpen Assetfinder timed out. $colourClose"
                         ;;
                 esac
             fi
         done
     done < $topLevelDomains
 
+    echo -e "$greenOpen Finished Passive Subddomain Enumeration $colourClose"
+
     if [[ $mode == "active" ]]; then
-        echo -e "$redOpen Starting active subdomain enumeration $redClose"
+        echo -e "$redOpen Starting Active Subdomain Enumeration $colourClose"
 
         #Taking too long. Find a way to reduce requests. Probably hitting API Rate Limiting. Added Timeout
         while IFS= read -r domain; do
-            echo -e "$redOpen Starting active subdomain enumeration on $domain $redClose"
+            echo -e "$redOpen Performing Active Subdomain Enumeration on $domain $colourClose"
 
             # Run amass enum with a timeout in the background
             timeout 5m amass enum -active -d $domain -silent | grep -v '[@*:]' | grep "$domain$" | anew $subdomains &
@@ -147,91 +162,106 @@ subdomainEnum(){
         done < $topLevelDomains
 
         #Custom Cert scraping
-        echo -e "$redOpen Starting SSL Cert Enumeration on $target $redClose"
+        echo -e "$redOpen Starting SSL Cert Enumeration $colourClose"
         nuclei -l $subdomains -t ssl/ssl-dns-names.yaml -silent | grep -oP '[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}' | sed 's/^\.//' | \
             grep -f $topLevelDomains | anew $subdomains | httprobe | anew $hosts &\
         nuclei -l $subdomains -t ssl/wildcard-tls.yaml -silent | grep -oP '\*\.[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}' | sed 's/^\*\.//' | \
             anew $unconfirmedTopLevelDomains
         wait    
+        echo -e "$greenOpen Finished Active Subddomain Enumeration $colourClose"
     fi
 }
 
 wayBackUrls(){
-    echo -e "$redOpen Starting Wayback Machine enumeration on $target $redClose"
+    echo -e "$redOpen Starting Wayback Machine Enumeration $colourClose"
     while IFS= read domain; do
         echo $domain | waybackurls | unfurl domains | anew $subdomains
     done < $subdomains
+    echo -e "$greenOpen Finished Wayback Machine Enumeration $colourClose"
 }
 
 githubEnum(){
-    echo -e "$redOpen Starting Github Scan on $target $redClose"
+    echo -e "$redOpen Starting Github Scan $colourClose"
     if [[ -n $gitToken ]]; then
         while IFS= read -r domain; do
         github-subdomains -d $domain -raw -t $gitToken | anew $subdomains
         done
     else
-        echo -e "$redOpen No GitToken. Skipping Github Enumeration $redClose"
+        echo -e "$redOpen No GitToken. Skipping Github Enumeration $colourClose"
     fi
+    echo -e "$greenOpen Finished Github Scan $colourClose"
 }
 
 httpResolve(){
-    echo -e "$redOpen Starting Http Resolve on $target $redClose"
+    echo -e "$redOpen Starting Http Resolve $colourClose"
     cat "$subdomains" | httprobe -c 20 | anew "$hosts" 
+    echo -e "$greenOpen Finished Http Resolve $colourClose"
 }
 
+#Check this code. Will probably need to unfurl and httpx 
 crawl(){
     if [[ $mode == "active" ]]; then
-       cat $hosts | hakrawler -insecure -d 5 | grep -oP 'https?://[^/ ]+' | grep -f $topLevelDomains | anew $hosts
+        echo -e "$redOpen Starting Crawling Hosts For Subdomains $colourClose"
+        cat $hosts | hakrawler -d 5 -insecure | grep -oP 'https?://[^/ ]+' | grep -f $topLevelDomains | unfurl domains | anew $subdomains | httprobe | anew $hosts
+        echo -e "$greenOpen Finished Crawling Hosts For Subdomains $colourClose"
     fi
 }
 
 getHeaders(){
     if [[ $mode == "active" ]]; then
-        cat $hosts | fff -d 5 -S -o $currentDirectory/InformationGathering/roots
-        gf meg-headers | anew $currentDirectory/InformationGathering/headers
-        sort $currentDirectory/InformationGathering/headers
+        echo -e "$redOpen Fetching Base Http Responses $colourClose"
+        cat $hosts | fff -d 5 -S -o $outputDirectory/InformationGathering/roots
+        gf meg-headers | anew $outputDirectory/InformationGathering/headers
+        sort $outputDirectory/InformationGathering/headers
+        echo -e "$greenOpen Finished Saving Base Http Responses $colourClose"
     fi
 }
 
 portScan(){
     if [[ -n $cidr && $mode == "active" ]]; then
-        echo -e "$redOpen Starting Port on $target $redClose"
+        echo -e "$redOpen Starting Port Scan With Masscan $colourClose"
         sudo masscan $cidr -p-
+        echo -e "$greenOpen Finished Port Scan With Masscan $colourClose"
     fi
 }
 
 #Resolves domains with interesting ports rather than IP addresses
 commonPortScan(){
     if [[ $mode == "active" ]]; then
-        echo -e "$redOpen Starting scan on subdomains to find http ports on $target $redClose"
+        echo -e "$redOpen Starting Common Http Port Scan $colourClose"
         nmap -T4 -iL $subdomains --script=http-title --open -p $commonPorts -Pn 
+        echo -e "$greenOpen Finished Common Http Port Scan $colourClose"
     fi
 }
 
 nucleiScan(){
     if [[ $mode == "active" ]]; then
-        echo -e "$redOpen Starting Nuclei Scans on $target $redClose"
+        echo -e "$redOpen Starting Nuclei Scans $colourClose"
         nuclei -l "$subdomains" -severity high,critical -rl 20 -c 2 -silent | anew $vulnerabilities &\
         nuclei -l "$hosts" -severity high,critical -rl 20 -c 2 -silent | anew $vulnerabilities
         wait
+        echo -e "$greenOpen Finished Nuclei Scans $colourClose"
     fi
 }
 
 iisDiscovery(){
     if [[ $mode == "active" ]]; then
-        echo -e "$redOpen Starting IIS Discovery on $target $redClose"
+        echo -e "$redOpen Starting IIS Discovery $colourClose"
         nuclei -l "$hosts" -t "./CustomTemplates/enhanced-iis-detection.yaml" -rl $rateLimit -silent | anew $iisServers
+        echo -e "$greenOpen Finished IIS Discovery $colourClose"
     fi
 }
 
 serviceScan(){
     if [[ $mode == "active" ]]; then
-        echo -e "$redOpen Starting Nmap Scan on $target $redClose"
+        echo -e "$redOpen Starting Service Scan With Nmap $colourClose"
         nmapFormat="${cidr//,/' '}"
         nmap $nmapFormat -sV -T4 -F -oG $nmapOutput
+        echo -e "$greenOpen Finished Service Scan With Nmap $colourClose"
 
-        echo -e "$redOpen Starting Credential Spraying on $target $redClose"
-        brutespray -f $nmapOutput
+        echo -e "$redOpen Starting Credential Spraying $colourClose"
+        brutespray -f $nmapOutput -q
+        echo -e "$greenOpen Finished Credential Spraying $colourClose"
     fi
 }
 
